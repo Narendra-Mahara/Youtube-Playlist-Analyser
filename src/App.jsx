dcd68b092566e6f7061c9f9aba485e2dfe1ebe2a
Narendra Mahara <narendramahara9868@gmail.com>
2025-07-09 13:30:39 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="h-svh w-full bg-slate-800 text-white">
+      <h1 className="text-center"> Youtube Playlist Analyser</h1>
+    </div>
+  );
+};
+
+export default App;
