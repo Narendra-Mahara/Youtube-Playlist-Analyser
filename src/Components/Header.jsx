8d26440db93ca8a654d00e5c4b753c746deeac11
Narendra Mahara <narendramahara9868@gmail.com>
2025-07-09 21:13:07 +0545

    add Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="bg-neutral-950 p-3 border-b-[0.5px] text-xl text-center sm:text-left">
+      <a href="/" className="inline-block w-fit">
+        <h1>Youtube Playlist Analyser</h1>
+      </a>
+    </div>
+  );
+};
+
+export default Header;
