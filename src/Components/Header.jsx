e18276cdc54dbd41413e9dd72a5bedc374829880
Narendra Mahara <narendramahara9868@gmail.com>
2025-07-10 14:56:07 +0545

    update Header component to enhance text styling and size

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="bg-neutral-950 p-3 border-b-[0.5px] text-xl text-center sm:text-left">
+    <div className="bg-neutral-950 p-3 border-b-[0.5px] text-2xl text-center sm:text-left font-bold ">
       <a href="/" className="inline-block w-fit">
         <h1>Youtube Playlist Analyser</h1>
       </a>
